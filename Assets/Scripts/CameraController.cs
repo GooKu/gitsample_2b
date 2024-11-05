fb022182a0a25fbb8fe8ca2f6b0fe8de28bac8ae
Goo <aaa@bbb.com>
2024-11-05 14:35:02 +0800

    to 0.1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+//v 0.1
         traget = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
